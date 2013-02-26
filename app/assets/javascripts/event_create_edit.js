b9c66c4918fdf1d1e2c0712d76d4fd005348958d
jessieay <jessie.a.young@gmail.com>
2013-02-26 09:30:07 -0800

    wip

--- app/assets/javascripts/event_create_edit.js
+++ app/assets/javascripts/event_create_edit.js
@@ -4,7 +4,14 @@
 //= require jquery-placeholder
 
 $(document).ready(function(){
+  window.onbeforeunload = function(e) {
+    return 'Dialog text here.';
+  };
+
   $('#new_event').submit(function(){
+    $('.cta').submit(function(){
+      window.beforeunload = null;
+     });
     $('.cta').prop('disabled', true);
   });
 });
